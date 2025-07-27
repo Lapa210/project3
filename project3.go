2ea73c7decec6d7db9b1ecb6cdb36c475fcd6e46
Lapa210 <IgorSmolyaninov@mail.ru>
2025-07-27 14:08:36 +0800

    program3

--- project3.go
+++ project3.go
@@ -2,7 +2,51 @@ package main
 
 import "fmt"
 
+func userInput() int {
+	userInp := 0
+	fmt.Println(`- 1. Посмотреть закладки
+- 2. Добавить закладку
+- 3. Удалить закладку
+- 4. Выход`)
+	fmt.Println("-----------------")
+	fmt.Print("Ввод:")
+	fmt.Scan(&userInp)
+	fmt.Println("-----------------")
+	return userInp
+}
+
+func logica(myMap map[string]string) {
+	flag := 0
+	for flag != 4 {
+		userInp := userInput()
+		switch {
+		case userInp == 1:
+			flag = 1
+			fmt.Println(myMap)
+		case userInp == 2:
+			key := ""
+			value := ""
+			flag = 2
+			fmt.Print("Введите ключ:")
+			fmt.Scan(&key)
+			fmt.Print("Введите значение:")
+			fmt.Scan(&value)
+			myMap[key] = value
+			fmt.Printf("Значение %s добавленно!\n", value)
+		case userInp == 3:
+			key := ""
+			flag = 3
+			fmt.Print("Введите ключ для удаления:")
+			fmt.Scan(&key)
+			delete(myMap, key)
+			fmt.Printf("Значение %s удаленно!\n", key)
+		case userInp == 4:
+			flag = 4
+		}
+	}
+}
+
 func main() {
-	a := 5
-	fmt.Println(a)
+	myMap := map[string]string{}
+	logica(myMap)
 }
